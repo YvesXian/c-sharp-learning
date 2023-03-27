f5320eed35898be9f1e6c6c7044d4af86531b742
Yves <yvesxianwu@gamil.com>
2023-03-28 00:05:17 +0800

    add learning code

---
+++ CallByReference_01/CallByReference_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallByReference_01
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine("a = " + a.ToString());
+            /*將變數 a 的"記憶體位置"丟進 ref_add 的 method 中。*/
+            ref_add(ref a);
+            Console.WriteLine("a = " + a.ToString());
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// -> 為什麼 ref_add() 需要加上 static ?
+        /// <- 在 C# 中，靜態成員是屬於類別而非物件的成員。
+        ///    這意味著如果您想在類別層級上共享一個變數，您需要使用 static 關鍵字來聲明該變數。
+        ///    這樣做可以確保所有類別的物件共享同一個變數，而不是每個物件都擁有自己的變數。
+        /// </summary>
+        /// <param name="b"></param>
+        static void ref_add(ref int b)
+        {
+            /*將記憶體位置丟進變數 b 中，因此如果變數 b 有做改變，就會連同影響丟進來的引數*/
+            b += 5;
+        }
+    }
+}
---
+++ Generic_01/Generic_01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>() { 1, 2, 3, 4, 5 };
+            List<double> doubleList = new List<double>() { 1.1, 2.2, 3.3, 4.4, 5.5 };
+
+            Console.WriteLine($"Sum of intList: {SumList(intList)}");
+            Console.WriteLine($"Sum of doubleList: {SumList(doubleList)}");
+            Console.ReadKey();
+        }
+
+        static T SumList<T>(List<T> list)
+        {
+            /*dynamic:
+             * 是 C# 中的一種型別，表示在執行期間可以動態決定型別。
+             * 
+             * default(T):
+             * 會依據 T 的型別而定，例如若 T 為 int 型別，則 default(T) 會是 0，
+             * 若 T 為 string 型別，則 default(T) 會是 null。*/
+            dynamic sum = default(T);
+            foreach (T item in list)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
+}
