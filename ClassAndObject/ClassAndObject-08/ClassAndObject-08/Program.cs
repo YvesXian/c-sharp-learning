5da4bd80e419247fed46e10037648e7f99f427ef
Yves <yvesxianwu@gamil.com>
2023-09-15 00:14:09 +0800

    Path sorting

---
+++ ClassAndObject/ClassAndObject-08/ClassAndObject-08/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ClassAndObject_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            float price;
+            string name;
+
+            Console.Write("Please input price: ");
+            input = Console.ReadLine();
+            price = float.Parse(input);
+
+            Console.Write("Please input object name: ");
+            input = Console.ReadLine();
+            name = input;
+
+            Items usb = new Items(price, name);
+            Console.Write("Please input object number: ");
+            input = Console.ReadLine();
+            usb.number = int.Parse(input);
+
+            Console.WriteLine("Total: " + usb.GetAmount() + "$");
+
+            Console.ReadKey();
+        }
+    }
+
+    interface Iface
+    {
+        float price { get; set; }
+        int number { get; set; }
+        string name { get; }
+
+        float GetAmount();
+    }
+
+    class Items : Iface
+    {
+        public float price { get; set; }
+        public int number { get; set; }
+        public string name { get; }
+
+        public Items(float p = 0, string str = "")
+        {
+            this.price = p;
+            this.name = str;
+            this.number = 0;
+        }
+
+        public float GetAmount()
+        {
+            return this.price * this.number;
+        }
+    }
+}
---
+++ ClassAndObject/ClassAndObject-09/ClassAndObject-09/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ClassAndObject_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Options Number: 1.一般情形 2.明確介面");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    MyClass1 my1 = new MyClass1();
+                    IfaceA fa1 = my1;
+                    IfaceB fb1 = my1;
+
+                    Console.WriteLine(fa1.ShowInfo());
+                    Console.WriteLine(fb1.ShowInfo());
+                    break;
+                case "2":
+                    MyClass2 my2 = new MyClass2();
+                    IfaceA fa2 = my2;
+                    IfaceB fb2 = my2;
+
+                    Console.WriteLine(fa2.ShowInfo());
+                    Console.WriteLine(fb2.ShowInfo());
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IfaceA
+    {
+        string ShowInfo();
+    }
+
+    interface IfaceB
+    {
+        string ShowInfo();
+    }
+
+    class MyClass1 : IfaceA, IfaceB
+    {
+        public string ShowInfo()
+        {
+            return "ShowInfo";
+        }
+    }
+
+    class MyClass2 : IfaceA, IfaceB
+    {
+        string IfaceA.ShowInfo()
+        {
+            return "ShowInfo for IfaceA";
+        }
+
+        string IfaceB.ShowInfo()
+        {
+            return "ShowInfo for IfaceB";
+        }
+    }
+}
